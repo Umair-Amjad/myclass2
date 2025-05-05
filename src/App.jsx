39c3dfcaf545f381b4c9b508ce3cdf4c6e35fcd5
Zaheerabbas <zabbas092002@gmail.com>
2025-05-05 23:43:55 +0500

    Add institute dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { store } from "./store";
 import AdminDashboard from "./pages/dashboard/AdminDashboard";
 import OrganizationManagement from "./pages/organization/OrganizationManagement";
 import OrganizationAdd from "./pages/organization/OrganizationAdd";
 import OrganizationDetails from "./pages/organization/OrganizationDetails";
+import EditOrganization from "./pages/organization/EditOrganization";
 import ViewInstitutes from "./pages/organization/ViewInstitutes";
-import InstituteDashboard from "./pages/dashboard/InstituteDashboard"; // Adjust the path as needed
+import InstituteDashboard from "./pages/dashboard/InstituteDashboard";
+import AddInstitute from "./pages/organization/AddInstitute";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/dashboard" element={<AdminDashboard />} />
-          <Route path="/organizations" element={<OrganizationManagement />} />
-          <Route path="/organizations/add" element={<OrganizationAdd />} />
-          <Route path="/organizations/:id" element={<OrganizationDetails />} />
-          <Route path="/institute/dashboard" element={<InstituteDashboard />} />
-          <Route
-            path="/organizations/:id/institutes"
-            element={<ViewInstitutes />}
-          />
-          {/* <Route path="/organizations/:id" element={<div>View Organization Details (To Be Implemented)</div>} />
-          <Route path="/organizations/:id/edit" element={<div>Edit Organization (To Be Implemented)</div>} />
-          <Route path="/organizations/:id/institutes" element={<div>View Institutes (To Be Implemented)</div>} />
-          <Route path="/organizations/add" element={<div>Add Organization (To Be Implemented)</div>} /> */}
-          <Route path="/" element={<AdminDashboard />} />
-          <Route path="*" element={<div>404 - Page Not Found</div>} />
-        </Routes>
-      </Router>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="/organizations" element={<OrganizationManagement />} />
+        <Route path="/organizations/add" element={<OrganizationAdd />} />
+        <Route path="/organizations/:id" element={<OrganizationDetails />} />
+        <Route path="/organizations/:id/edit" element={<EditOrganization />} />
+        <Route path="/institute/dashboard" element={<InstituteDashboard />} />
+        <Route path="/institutes/add" element={<AddInstitute />} />
+        <Route
+          path="/organizations/:id/institutes"
+          element={<ViewInstitutes />}
+        />
+        {/* <Route path="/" element={<AdminDashboard />} /> */}
+        <Route path="*" element={<div>404 - Page Not Found</div>} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
